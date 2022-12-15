40ead3ca76d91d3f10d0132ad0bdc29ad43e02e3
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-12-15 14:01:09 +0100

    add test for grid

--- test/main.c
+++ test/main.c
@@ -5,12 +5,40 @@ static Grid *g = NULL;
 
 void testCreate() {
     g=Grid_create();
-    CU_ASSERT(g!=NULL);
+    CU_ASSERT(g!=NULL)
+}
+
+void testPlaceToken() {
+    CU_ASSERT(g!=NULL)
+    g->placeToken(g,0,1);
+    CU_ASSERT(Grid_getErrorCode()==0)
+    g->placeToken(g,1,2);
+    CU_ASSERT(Grid_getErrorCode()==0)
+    g->placeToken(g,2,1);
+    CU_ASSERT(Grid_getErrorCode()==0)
+    g->placeToken(g,3,2);
+    CU_ASSERT(Grid_getErrorCode()==0)
+    g->placeToken(g,4,1);
+    CU_ASSERT(Grid_getErrorCode()==0)
+    g->placeToken(g,5,2);
+    CU_ASSERT(Grid_getErrorCode()==0)
+    g->placeToken(g,6,1);
+    CU_ASSERT(Grid_getErrorCode()==0)
+    g->placeToken(g,6,0);
+    CU_ASSERT(Grid_getErrorCode()!=0)
+    g->placeToken(g,7,1);
+    CU_ASSERT(Grid_getErrorCode()!=0)
+}
+
+void testReset() {
+    CU_ASSERT(g!=NULL)
+    g->reset(g);
+    CU_ASSERT(Grid_getErrorCode()==0)
 }
 
 void testDestroy() {
     g->destroy(g);
-    CU_ASSERT(Grid_getErrorCode()==0);
+    CU_ASSERT(Grid_getErrorCode()==0)
 }
 
 int main() {
@@ -19,13 +47,15 @@ int main() {
         return CU_get_error();
     grid = CU_add_suite("Grid", NULL, NULL);
     if (!grid) {
-        fprintf(stderr,"Error : add suite failed !\n");
+        fprintf(stderr,"Error : add grid suite failed !\n");
         goto quit;
     }
     if ((NULL == CU_add_test(grid, "create", testCreate)) ||
+        (NULL == CU_add_test(grid, "placeToken", testPlaceToken)) ||
+        (NULL == CU_add_test(grid, "reset", testReset)) ||
         (NULL == CU_add_test(grid, "destroy", testDestroy)))
     {
-        fprintf(stderr,"Error : add suite failed !\n");
+        fprintf(stderr,"Error : add test for grid suite failed !\n");
         goto quit;
     }
     CU_basic_set_mode(CU_BRM_VERBOSE);
